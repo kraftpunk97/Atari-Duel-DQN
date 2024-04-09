e8a1bf751526ec1430f7e6c1b3498181225671fe
kraftpunk97 <kartikeygupta9097@gmail.com>
2024-04-09 05:54:51 -0500

    Initial commit

---
+++ rl_collections/dqn.py
@@ -0,0 +1,14 @@
+from torch.nn import Module
+from torch.nn import Conv2d
+
+
+class DQN(Module):
+    def __init__(self):
+        super(DQN, self).__init__() # Initialize the parent constructor
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
